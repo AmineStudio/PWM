ffc91ae5f09452e6cffca8f69941e7466357351f
miguelalonsocorimanyarojas-boop <miguelalonsocorimanyarojas@gmail.com>
2026-03-19 20:56:35 +0000

    SOBRE NOSOTROS, LOGIN
    Rediseño de las páginas Sobre nosotros y Login, adaptándose al diseño del sitio web

---
+++ java/login.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const container = document.getElementById('container');
+    const registerBtn = document.getElementById('register');
+    const loginBtn = document.getElementById('login');
+
+    if(!container || !registerBtn || !loginBtn) return;
+
+    registerBtn.addEventListener('click', () => {
+        container.classList.add("active");
+    });
+
+    loginBtn.addEventListener('click', () => {
+        container.classList.remove("active");
+    });
+
+});
