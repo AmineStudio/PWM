7dbc915813b3158b99d6522fb4dbfa831ca62df6
miguelalonsocorimanyarojas-boop <miguelalonsocorimanyarojas@gmail.com>
2026-04-12 13:54:42 +0100

    feat: migración de la página alérgenos

--- src/app/pages/alergenos/alergenos.component.ts
+++ src/app/pages/alergenos/alergenos.component.ts
@@ -1,9 +1,50 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Alergeno {
+  nombre: string;
+  descripcion: string;
+  imagen: string;
+}
 
 @Component({
   selector: 'app-alergenos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './alergenos.component.html',
-  styleUrl: './alergenos.component.css',
+  styleUrls: ['./alergenos.component.css'],
 })
-export class AlergenosComponent {}
+export class AlergenosComponent {
+  public alergenos: Alergeno[] = [
+    {
+      nombre: 'Gluten',
+      descripcion: 'Presente en panes y rebozados crujientes.',
+      imagen: 'assets/img/alergenos/gluten.png',
+    },
+    {
+      nombre: 'Lácteos',
+      descripcion: 'Quesos fundidos y salsas especiales de la casa.',
+      imagen: 'assets/img/alergenos/lacteos.png',
+    },
+    {
+      nombre: 'Frutos Secos',
+      descripcion: 'Posibles trazas en postres y aceites seleccionados.',
+      imagen: 'assets/img/alergenos/frutossecos.png',
+    },
+    {
+      nombre: 'Huevo',
+      descripcion: 'En mayonesa casera y nuestros panes brioche.',
+      imagen: 'assets/img/alergenos/huevo.png',
+    },
+    {
+      nombre: 'Soja',
+      descripcion: 'Presente en marinadas para carnes y vegetales.',
+      imagen: 'assets/img/alergenos/soja.png',
+    },
+    {
+      nombre: 'Pescado',
+      descripcion: 'Salsa César y opciones marinas del menú.',
+      imagen: 'assets/img/alergenos/pescado.png',
+    },
+  ];
+}
