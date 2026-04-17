fa6776e5e9bca420123e56698e5f778f8984d77c
Amine <amine.saidani.101@gmail.com>
2026-04-17 15:32:24 +0100

    configuracion de firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBJ47XX0hzgc68uTUZi44Kdw9q2_Gi5VAs',
+    authDomain: 'tahmking-9ade5.firebaseapp.com',
+    projectId: 'tahmking-9ade5',
+    storageBucket: 'tahmking-9ade5.firebasestorage.app',
+    messagingSenderId: '303489094590',
+    appId: '1:303489094590:web:5eb797a2e50db489825fef',
+    measurementId: 'G-KP2MR7P5D2',
+  },
+};
